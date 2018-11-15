d29684d788e0aaf7e9a05d9d0df490138ad67c87
notiocide <stuhlmeier02@gmail.com>
2018-11-15 11:51:11 +0100

    Add level 2
    Signed-off-by: notiocide <stuhlmeier02@gmail.com>

--- src/training/mars/Util.kt
+++ src/training/mars/Util.kt
@@ -1,5 +1,9 @@
 package training.mars
 
+import java.awt.Toolkit
+import java.awt.datatransfer.StringSelection
+
+
 fun getTurnRadius(wheelBase: Double, steeringAngle: Double): Double {
     return wheelBase / Math.sin(Math.toRadians(steeringAngle))
 }
@@ -8,4 +12,28 @@ fun Double.format() = String.format("%.2f", this)
 
 fun printf(format: String, vararg args: Any?) {
     System.out.printf(format, *args)
+}
+
+fun output(vararg values: Any) {
+    val message = values.map {
+        when (it) {
+            is Double -> it.format()
+            else -> it
+        }
+    }.joinToString(" ")
+
+    clipboard(message)
+    println(message)
+}
+
+fun Double.positiveAngle(): Double {
+    var copy = this
+    while (copy < 0) copy += 360
+    return copy
+}
+
+fun clipboard(string: String) {
+    val selection = StringSelection(string)
+    val clipboard = Toolkit.getDefaultToolkit().getSystemClipboard()
+    clipboard.setContents(selection, selection)
 }
